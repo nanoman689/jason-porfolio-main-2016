35b84ac92e2c1d21ee689753597411164155c833
Jason Thomas <nanoman689@gmail.com>
2016-06-08 19:48:02 -0700

    06-08-16_FirstChangesfromOlex

--- js/jq.js
+++ js/jq.js
@@ -1,19 +1,4 @@
-$(document).ready(function() {
-    /*
-    $('.twoBox .col-2-3').height($('.twoBox .col-1-3').height())
-    */
-    
-    $('.aButton').click(function(){
-        $("div").show("slow");
-        console.log("click!");
-    });
-    
-    $('.bButton').click(function(){
-        $("div.contactBox").hide(600);
-        console.log("clickAgain!");
-    });
-    
-})
+
 
 
 /* Have a div that hides and shows the div */
--- js/script.js
+++
@@ -1,3 +0,0 @@
-function myContactWindow() {
-    window.open("contact.html", "_blank", "toolbar=yes,scrollbars=no,resizable=no,top=500,left=500,width=680,height=400");
-}
